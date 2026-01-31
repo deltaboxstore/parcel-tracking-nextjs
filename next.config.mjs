126ed7b470ad29e44b825a90f3e4e34871098b2b
DeltaboxStore <deltaboxstore.custsvc@gmail.com>
2026-01-31 12:18:08 +0800

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'deltaboxstore.com',
+          },
+        ],
+        destination: 'https://rhythmnexus.org/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
