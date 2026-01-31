ba02aedd4ee96d21ad0330317e4922250463037a
DeltaboxStore <deltaboxstore.custsvc@gmail.com>
2026-01-31 16:01:43 +0800

    initial

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+module.exports = {
+  reactStrictMode: true,
   async redirects() {
     return [
       {
@@ -18,5 +16,3 @@ const nextConfig = {
     ];
   },
 };
-
-export default nextConfig;
